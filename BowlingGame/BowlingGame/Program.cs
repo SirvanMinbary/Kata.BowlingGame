13c2a86c14a12731ea85e4f0477e987f31f35bdc
Sirvan Minbary <s.minbary@gmail.com>
2018-10-13 21:59:47 +0200

    added game states and frame advancement

--- BowlingGame/BowlingGame/Program.cs
+++ BowlingGame/BowlingGame/Program.cs
@@ -17,12 +17,32 @@ namespace BowlingGame
     public class Game
     {
         public readonly int MaxFrames = 10;
-        int currentFrame = 0;
-        int previousKnockedDownPins = 0;
-        int currentOpportunity = 0;
+        int currentFrameCount = 0;
+        public GameState CurrentState
+        {
+            get
+            {
+                if (currentFrameCount == MaxFrames - 1)
+                {
+                    return GameState.LastFrame;
+                }
+
+                if (currentFrameCount < MaxFrames)
+                {
+                    return GameState.InProgress;
+                }
+
+                if (currentFrameCount >= MaxFrames)
+                {
+                    return GameState.Finished;
+                }
+
+                throw new InvalidOperationException("Unknown game state");
+            }
+        }
 
         public List<Frame> Frames { get; set; } = new List<Frame>();
-        public LastFrame LastFrame { get; set; }
+        public LastFrame LastFrame { get; set; } = new LastFrame();
 
         public Game()
         {
@@ -40,9 +60,35 @@ namespace BowlingGame
             {
                 throw new ArgumentException("Can not knock down more than 10 pins");
             }
+
+            if (CurrentState == GameState.Finished)
+            {
+                throw new InvalidOperationException("Maximum amount of frames has been reached");
+            }
+
+            if (CurrentState == GameState.InProgress)
+            {
+                var currentFrame = Frames[currentFrameCount];
+                currentFrame.AddRoll(pins);
+
+                if (currentFrame.AdvanceNextFrame)
+                {
+                    currentFrameCount++;
+                }
+            }
+
+            if (CurrentState == GameState.LastFrame)
+            {
+                LastFrame.AddRoll(pins);
+                if (LastFrame.FinishGame)
+                {
+                    currentFrameCount++;
+                }
+            }
+
         }
 
-        int Score()
+        public int Score()
         {
             throw new NotImplementedException();
         }
@@ -61,11 +107,80 @@ namespace BowlingGame
         public int? FirstRoll { get; set; }
         public int? SecondRoll { get; set; }
         public ScoreModifier Modifier { get; set; } = ScoreModifier.None;
+        public bool AdvanceNextFrame
+        {
+            get
+            {
+                if (Modifier == ScoreModifier.Strike || Modifier == ScoreModifier.Spare)
+                {
+                    return true;
+                }
+                else if (FirstRoll.HasValue && SecondRoll.HasValue)
+                {
+                    return true;
+                }
+
+                return false;
+            }
+        }
+
+        public virtual void AddRoll(int pins)
+        {
+            if (!FirstRoll.HasValue)
+            {
+                FirstRoll = pins;
+
+                if (pins == 10)
+                {
+                    Modifier = ScoreModifier.Strike;
+                }
+            }
+            else
+            {
+                SecondRoll = pins;
+
+                if (FirstRoll.Value + SecondRoll.Value == 10)
+                {
+                    Modifier = ScoreModifier.Spare;
+                }
+            }
+        }
     }
 
     public class LastFrame : Frame
     {
         public int? ThirdRoll { get; set; }
+        public bool FinishGame
+        {
+            get
+            {
+                return ThirdRoll.HasValue;
+            }
+        }
+
+        public override void AddRoll(int pins)
+        {
+            if (!FirstRoll.HasValue)
+            {
+                FirstRoll = pins;
+                if (pins == 10)
+                {
+                    Modifier = ScoreModifier.Strike;
+                }
+            }
+            else if (!SecondRoll.HasValue && Modifier != ScoreModifier.Strike)
+            {
+                SecondRoll = pins;
+                if (FirstRoll.Value + SecondRoll.Value == 10)
+                {
+                    Modifier = ScoreModifier.Spare;
+                }
+            }
+            else
+            {
+                ThirdRoll = pins;
+            }
+        }
     }
 
     public enum ScoreModifier
@@ -73,5 +188,13 @@ namespace BowlingGame
         None,
         Spare,
         Strike
+    }
+
+    public enum GameState
+    {
+        Started,
+        InProgress,
+        LastFrame,
+        Finished
     }
 }
--- BowlingGame/BowlingGameTests/GameTests.cs
+++ BowlingGame/BowlingGameTests/GameTests.cs
@@ -11,7 +11,13 @@ namespace BowlingGameTests
     [TestFixture]
     public class GameTests
     {
-        private Game game = new Game();
+        private Game game;
+
+        [SetUp]
+        public void SetUp()
+        {
+            game = new Game();
+        }
 
         [TestCase(-1, "Can not knock down less than zero pins")]
         [TestCase(11, "Can not knock down more than 10 pins")]
@@ -26,6 +32,84 @@ namespace BowlingGameTests
         {
             var frameCount = game.Frames.Count + 1;
             Assert.AreEqual(frameCount, game.MaxFrames);
+        }
+
+        [Test]
+        public void Game_Strike_IsRegistered()
+        {
+            game.Roll(10);
+            var firstFrameModifier = game.Frames[0].Modifier;
+            Assert.That(firstFrameModifier == ScoreModifier.Strike, "Strike was not registered");
+        }
+
+        [Test]
+        public void Game_AllStrikesReached()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                game.Roll(10);
+            }
+
+            Assert.IsTrue(game.Frames.TrueForAll(x => x.FirstRoll.Value == 10 && !x.SecondRoll.HasValue));
+            Assert.IsTrue(game.LastFrame.FirstRoll.Value == 10);
+            Assert.IsTrue(!game.LastFrame.SecondRoll.HasValue);
+            Assert.IsTrue(game.LastFrame.ThirdRoll.Value == 10);
+        }
+
+        [Test]
+        public void Game_Spare_IsRegistered()
+        {
+            game.Roll(5);
+            game.Roll(5);
+            var firstFrameModifier = game.Frames[0].Modifier;
+            Assert.That(firstFrameModifier == ScoreModifier.Spare, "Spare was not registered");
+        }
+
+        [Test]
+        public void Game_Frame_Advances()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                game.Roll(0);
+            }
+            var secondFrame = game.Frames[1];
+            Assert.That(secondFrame.FirstRoll.HasValue);
+        }
+
+        [Test]
+        public void Game_LastFrame_Registered()
+        {
+            for (int i = 0; i < 18; i++)
+            {
+                game.Roll(0);
+            }
+
+            Assert.That(game.CurrentState == GameState.LastFrame);
+        }
+
+        [Test]
+        public void Game_LastFrameThirdRollReached()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                game.Roll(0);
+            }
+
+            Assert.That(game.LastFrame.ThirdRoll.HasValue);
+        }
+
+        [Test]
+        public void Game_AllFramesReached()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                game.Roll(0);
+            }
+
+            var frames = game.Frames;
+            var lastFrame = game.LastFrame;
+            Assert.IsTrue(frames.TrueForAll(x => x.FirstRoll.HasValue && x.SecondRoll.HasValue));
+            Assert.IsTrue(lastFrame.FirstRoll.HasValue && lastFrame.SecondRoll.HasValue && lastFrame.ThirdRoll.HasValue);
         }
     }
 }
