01bfe5589f915705a49c0e191ddc1f39b18f87ad
Sirvan Minbary <s.minbary@gmail.com>
2018-10-13 16:23:12 +0200

    init

---
+++ BowlingGame/BowlingGame/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class Game
+    {
+        void Roll(int pins)
+        {
+
+        }
+
+        int Score()
+        {
+
+        }
+    }
+}
